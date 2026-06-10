0a7b537808475d9db5054719e7ad92f63e236452
Hansadhwaja <hansadhwajabiswal@gmail.com>
2026-06-10 21:08:29 +0530

    Added categories to add expense modal from dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,24 @@
 import PageHeader from "@/components/common/Header/PageHeader"
 import AddExpenseModal from "@/components/expense/Modal/AddExpenseModal"
-
+import { getCategoriesAction } from "@/lib/actions/category.actions"
 
 const DashboardPage = async () => {
-    return (
-        <div>
-            <PageHeader
-                title="Dashboard"
-                description="Welcome Back John 👋.Here's your Financial overview."
-                others={
-                    <div>
-                        <AddExpenseModal />
-                    </div>
-                }
-            />
-        </div>
-    )
+  const categoriesResponse = await getCategoriesAction()
+
+  const categories = categoriesResponse.data?.categories ?? []
+  return (
+    <div>
+      <PageHeader
+        title="Dashboard"
+        description="Welcome Back John 👋.Here's your Financial overview."
+        others={
+          <div>
+            <AddExpenseModal categories={categories} />
+          </div>
+        }
+      />
+    </div>
+  )
 }
 
 export default DashboardPage
